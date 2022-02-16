eeb16cbe4f94b7c6b06a3ab1ffa16a580598aa50
Danielle Navarro <djnavarro@protonmail.com>
2022-02-16 17:29:15 +1100

    fixes font spec and aligns colours

--- source/logo-functions.R
+++ source/logo-functions.R
@@ -1,4 +1,4 @@
-# generate Apache Arrow logos programmatically
+# generate Womxn at Voltron Data logos programmatically
 
 library(ggplot2)
 library(dplyr)
@@ -9,23 +9,23 @@ library(showtext)
 
 
 export_logo <- function(plot, path, height = 6, width = 6, background = NULL) {
-  ggsave(path, plot, width = width, height = height, dpi = 300, bg = background)
+  ggsave(path, plot, width = width, height = height, dpi = 96, bg = background)
 }
 
 generate_trapezoid <- function(vw) {
   wd <- .075
   tribble(
-          ~x,    ~y,   ~part,
-           0,     1, "outer",
-          .5,     1, "outer",
-         .75,    .5, "outer",
-          .5,     0, "outer",
-           0,     1, "outer",
-          .5,  1-wd, "inner",
+    ~x,    ~y,   ~part,
+    0,     1, "outer",
+    .5,     1, "outer",
+    .75,    .5, "outer",
+    .5,     0, "outer",
+    0,     1, "outer",
+    .5,  1-wd, "inner",
     .75-wd/2,    .5, "inner",
-          .5,    wd, "inner",
+    .5,    wd, "inner",
     .25+wd/2,    .5, "inner",
-          .5,  1-wd, "inner"
+    .5,  1-wd, "inner"
   ) %>% 
     mutate(
       x = x * vw,
@@ -46,14 +46,14 @@ generate_bracket <- function(vw) {
   l <- wd * sin(pi/3)
   s <- wd * cos(pi/3)
   tribble(
-        ~x,      ~y,
-        -s,       1,
-         1,       1,
-        .5,       0,
-      .5-s,       l,
-      1-wd-s,   1-l,
-      0, 1-l,
-      -s, 1
+    ~x,      ~y,
+    -s,       1,
+    1,       1,
+    .5,       0,
+    .5-s,       l,
+    1-wd-s,   1-l,
+    0, 1-l,
+    -s, 1
   ) %>% 
     mutate(
       id = "bracket",
@@ -66,32 +66,12 @@ generate_bracket <- function(vw) {
 
 
 generate_slash <- function(vw) {
-  wd <- .05
-  tribble(
-    ~x,    ~y, 
-     0,     1, 
-    wd,     1, 
-  .5 + wd/2,  wd * sqrt(3/2) / vw,
-    .5,     0, 
-     0,     1
-  ) %>% 
-    mutate(
-      x = x * vw - .6,
-      id = "slash"
-    )
-  
-}
-
-generate_tickslash <- function(vw) {
-  wd <- .04
-  scf <- wd * sqrt(3/2) / vw
+  wd <- .01
   tribble(
     ~x,    ~y, 
     0,     1, 
     wd,     1, 
-    .5, 2*scf,
-    .5 + wd, 4*scf,
-    .5 + 1.5*wd, 3*scf,
+    .5 + wd/2,  wd * sqrt(3/2) / vw,
     .5,     0, 
     0,     1
   ) %>% 
@@ -102,31 +82,42 @@ generate_tickslash <- function(vw) {
   
 }
 
-
-
-
 generate_logotype <- function() {
   tibble(
-    x = c(1.5, 1.5),
+    x = c(2, 2),
     y = c(.5, 1),
-    text = c("V O L T R O N    D A T A", "W O M X N    A T"),
+    text = c("V O L T R O N   D A T A", "W O M X N    A T"),
     font = c("archerus", "archerus"),
-    weight = c("bold", "bold"), # <- in the hacked folder this is actually semibold because ggplot2 
-    size = c(50, 25), # <- the intent is really a 3:1 ratio in font size
+    weight = c("bold", "italic"),  # ... these are lies!
+    size = c(20, 10),
     hjust = c("left", "left"),
     vjust = c("center", "top")
   )
 } 
 
 
+read_shade <- function(colour) {
+  colour_code <- colour
+  if(colour_code == "primarygreen") colour_code <- "#005050"
+  if(colour_code == "primarygrey") colour_code <- "#d9d8d6"
+  return(colour_code)
+}
+
 specify_logo_horizontal <- function(colour = "black", background = "white", crop = FALSE) {
   
   # load fonts
   showtext_auto()
   
+  acherus <- function(name) {
+    here::here("font", paste0("acherusgrotesque-", name, ".otf"))
+  }
+  
+  # there may be some lies here...
   font_add(
     family = "archerus", 
-    regular = here::here("font", "acherusgrotesque-regular.otf")
+    regular = acherus("regular"),
+    bold = acherus("semibold"), 
+    italic = acherus("light")
   )
   
   # create the components
@@ -136,34 +127,44 @@ specify_logo_horizontal <- function(colour = "black", background = "white", crop
   slash <- generate_slash(v_width)
   bracket <- generate_bracket(v_width)
   
+  # tweak location of the logo
+  x_shift <- .2
+  trapezoid <- trapezoid %>% mutate(x = x + x_shift)
+  slash <- slash %>% mutate(x = x + x_shift)
+  bracket <- bracket %>% mutate(x = x + x_shift)
+  
   # specify plot limits
-  x_limit <- c(-2, 8) # w: 10
-  y_limit <- c(-2, 3) # h: 5 
+  x_limit <- c(-4, 12) # w: 16
+  y_limit <- c(-3.5, 4.5) # h: 8
   
   # specify line width
   line_width <- 2
-
+  
+  # specify colours
+  colour_code <- read_shade(colour)
+  background_code <- read_shade(background)
+  
   # construct plot
   pic <- ggplot() +
     geom_polygon(
       data = trapezoid,
       mapping = aes(x, y, group = id, subgroup = part),
-      fill = colour,
-      colour = colour,
+      fill = colour_code,
+      colour = colour_code,
       size = .5
     ) +
     geom_polygon(
       data = slash,
       mapping = aes(x, y, group = id),
-      fill = colour,
-      colour = colour,
-      size = .5
+      fill = colour_code,
+      colour = colour_code,
+      size = .1
     ) +
     geom_polygon(
       data = bracket,
       mapping = aes(x, y, group = id),
-      colour = colour,
-      fill = colour,
+      colour = colour_code,
+      fill = colour_code,
       size = .5,
     ) +
     geom_text(
@@ -177,13 +178,13 @@ specify_logo_horizontal <- function(colour = "black", background = "white", crop
         hjust = hjust,
         vjust = vjust
       ),
-      colour = colour
+      colour = colour_code
     ) +
     coord_equal() +
     scale_size_identity() +
     theme_void() +
     theme(panel.background = element_rect(
-      fill = background, colour = background
+      fill = background_code, colour = background_code
     )) + 
     scale_x_continuous(limits = x_limit, expand = c(0, 0)) +
     scale_y_continuous(limits = y_limit, expand = c(0, 0)) +
@@ -213,16 +214,16 @@ create_logo_horizontal <- function(dir, colour, background, format = "png") {
   export_logo(
     plot = pic,
     path = file.path(dir, fname), 
-    background = background, 
-    height = 6,
-    width = 12
+    background = read_shade(background), 
+    height = 12,
+    width = 24
   )
 }
 
 create_logo_horizontal(
   dir = here::here("img"),
-  colour = "black",
-  background = "white"
+  colour = "white",
+  background = "primarygreen"
 )
 
 
